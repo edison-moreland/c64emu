d920ad8e2ffd4f8104ec26847a0e1baa01b6a54e
Edison Moreland <edison@devdude.net>
2022-04-03 02:02:10 -0700

    Print instruction, formatted

--- cpu/debugging.go
+++ cpu/debugging.go
@@ -1,17 +1,40 @@
 package cpu
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+	"strings"
+)
 
-func (c *CPU) debug() {
-	nextInstruction, err := Decode(c.Memory.ReadByte(c.PC))
+func printInstruction(raw []byte) {
+	instruction, err := Decode(raw[0])
 	if err != nil {
-		fmt.Printf("Invalid opcode: %02X\n", c.Memory.ReadByte(c.PC))
-	} else {
-		fmt.Printf("%-5s | %s\n",
-			nextInstruction.Category.String(),
-			nextInstruction.Mnemonic.String())
+		fmt.Printf("Invalid opcode: %02X\n", raw[0])
+		return
 	}
 
+	addressingModeStr := instruction.AddressingMode.String()
+	if instruction.Size == 2 {
+		var bytestr = fmt.Sprintf("$%02X", raw[1])
+		addressingModeStr = strings.ReplaceAll(addressingModeStr, "d8", bytestr)
+		addressingModeStr = strings.ReplaceAll(addressingModeStr, "a8", bytestr)
+		addressingModeStr = strings.ReplaceAll(addressingModeStr, "r8", bytestr)
+	}
+	if instruction.Size == 3 {
+		var word = binary.LittleEndian.Uint16(raw[1:])
+		addressingModeStr = strings.ReplaceAll(addressingModeStr, "a16", fmt.Sprintf("$%04X", word))
+	}
+
+	fmt.Printf("%-5s | %s %s\n",
+		instruction.Category.String(),
+		instruction.Mnemonic.String(),
+		addressingModeStr,
+	)
+
+}
+
+func (c *CPU) debug() {
+	printInstruction(c.Memory.Read(c.PC, 3))
 	fmt.Printf("PC: %04X S: %02X A: %02X X: %02X Y: %02X \n", c.PC, c.S, c.A, c.X, c.Y)
 	fmt.Printf("N: %v V: %v -: 0 B: %v D: %v I: %v Z: %v C: %v \n",
 		c.flag(Status_Negative),
@@ -21,16 +44,18 @@ func (c *CPU) debug() {
 		c.flag(Status_InterruptDisable),
 		c.flag(Status_Zero),
 		c.flag(Status_Carry))
+
+	next8Bytes := c.Memory.Read(c.PC, 8)
 	fmt.Printf("%04X: %02X %02X %02X %02X %02X %02X %02X %02X\n",
 		c.PC,
-		c.Memory.ReadByte(c.PC),
-		c.Memory.ReadByte(c.PC+1),
-		c.Memory.ReadByte(c.PC+2),
-		c.Memory.ReadByte(c.PC+3),
-		c.Memory.ReadByte(c.PC+4),
-		c.Memory.ReadByte(c.PC+5),
-		c.Memory.ReadByte(c.PC+6),
-		c.Memory.ReadByte(c.PC+7))
+		next8Bytes[0],
+		next8Bytes[1],
+		next8Bytes[2],
+		next8Bytes[3],
+		next8Bytes[4],
+		next8Bytes[5],
+		next8Bytes[6],
+		next8Bytes[7])
 	fmt.Printf("Interrupt Pending: %-5v Vector: %04X\n", c.InterruptPending, c.InterruptVector)
 	fmt.Printf("> ")
 	fmt.Scanln()
