cb8305173e99bce924f875222715ae5f3433f08f
Edison Moreland <edison@devdude.net>
2022-04-06 15:28:04 -0700

    Simplify rom loading

--- main.go
+++ main.go
@@ -11,32 +11,27 @@ import (
 //go:embed roms/*.bin
 var roms embed.FS
 
-const (
-	KernalRom    = "roms/kernal.901227-03.bin"
-	BasicRom     = "roms/basic.901226-01.bin"
-	CharacterRom = "roms/characters.901225-01.bin"
+var (
+	Roms = []struct {
+		Path       string
+		Start, End uint16
+	}{
+		{"roms/kernal.901227-03.bin", 0xE000, 0xFFFF},
+		{"roms/basic.901226-01.bin", 0xA000, 0xBFFF},
+		{"roms/characters.901225-01.bin", 0xD000, 0xDFFF},
+	}
 )
 
 func main() {
 	mem := memory.New()
 
-	kernalRom, err := memory.NewRomFromFile(roms, KernalRom)
-	if err != nil {
-		panic(err)
+	for _, romInfo := range Roms {
+		rom, err := memory.NewRomFromFile(roms, romInfo.Path)
+		if err != nil {
+			panic(err)
+		}
+		mem.AddDevice(romInfo.Start, romInfo.End, rom)
 	}
-	mem.AddDevice(0xE000, 0xFFFF, kernalRom)
-
-	basicRom, err := memory.NewRomFromFile(roms, BasicRom)
-	if err != nil {
-		panic(err)
-	}
-	mem.AddDevice(0xA000, 0xBFFF, basicRom)
-
-	characterRom, err := memory.NewRomFromFile(roms, CharacterRom)
-	if err != nil {
-		panic(err)
-	}
-	mem.AddDevice(0xD000, 0xDFFF, characterRom)
 
 	memoryContext, cancel := context.WithCancel(context.Background())
 	defer cancel()
